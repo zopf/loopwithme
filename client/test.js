d6b44c89d61111707d652918f69c1e08977065a4
jonathanv3232 <jonathanv3232@gmail.com>
2015-06-20 20:22:12 +0000

    jquery hover functionality added

---
+++ client/test.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    function changeColorGrid(){
+        $("#box1").hover(function(){
+            $(this).css("background-color", "lightblue");
+            },
+        $("#box1").hover(function(){
+            $(this).css("background-color", "lightblue");
+            },
+        $("#box1").hover(function(){
+            $(this).css("background-color", "lightblue");
+            },
+        
+        $("#box1").hover(function(){
+            $(this).css("background-color", "lightblue");
+            },
+        $("#box1").hover(function(){
+            $(this).css("background-color", "lightblue");
+            },
+        $("#box1").hover(function(){
+            $(this).css("background-color", "lightblue");
+        }
+    };
+    changeColorGrid();
+});
